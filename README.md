980c5d4ccf3916a5aeca38bc8bb3eca734bfc092
xAlpharax <42233094+xAlpharax@users.noreply.github.com>
2024-08-12 20:14:27 +0300

    Made persistent light/dark theme with toggles and picked basic coloring for the application.
     Changes to be committed:
            modified:   .idea/libraries/Dart_Packages.xml
            modified:   .idea/libraries/Flutter_Plugins.xml
            modified:   README.md
            modified:   lib/main.dart
            modified:   pubspec.yaml

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ fuck god damn
 https://stackoverflow.com/questions/54483617/flutter-daemon-terminated-when-i-try-to-launch-my-app fuck me
 
 Flutter To-Do list app with firebase and sqlite backend yadayada.
+
+material design: auth screen -> dashboard -> form for adding and editing entries
+                                          -> settings (icon) screen with theme switch
+light / dark mode toggle with persistance storage -- DONE
+
+Using
+
+firebase_core
+firebase_auth
+cloud_firestore
+firebase_messaging
+
+GetX
+GetX_storage
+
+Dio
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
+// Controller for managing the theme mode
+class ThemeController extends GetxController {
+  // Use GetStorage to persist theme data
+  final GetStorage _box = GetStorage();
+  final String _key = 'isDarkTheme';
+
+  // RxBool for tracking whether the dark theme is enabled
+  var isDarkTheme = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    // Load the theme from storage or use the default theme (light)
+    isDarkTheme.value = _loadThemeFromBox();
+  }
+
+  // Load the theme from GetStorage
+  bool _loadThemeFromBox() {
+    return _box.read(_key) ?? false; // Return false if the key doesn't exist
+  }
+
+  // Save the theme to GetStorage and toggle the theme
+  void toggleTheme() {
+    isDarkTheme.value = !isDarkTheme.value;
+    _saveThemeToBox(isDarkTheme.value);
+  }
+
+  // Save the theme preference to GetStorage
+  void _saveThemeToBox(bool isDarkTheme) {
+    _box.write(_key, isDarkTheme);
+  }
+}
+
+void main() async {
+  await GetStorage.init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Instantiate the ThemeController using Get.put to make it globally available
+  final ThemeController themeController = Get.put(ThemeController());
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    return Obx(() {
+      return GetMaterialApp(
+        title: 'Flutter GetX Theme Demo',
+        theme: ThemeData.light(), // Light theme
+        darkTheme: ThemeData.dark(), // Dark theme
+        themeMode: themeController.isDarkTheme.value ? ThemeMode.dark : ThemeMode.light, // ternary go brr
+        home: HomeScreen(),
+        debugShowCheckedModeBanner: false,
+      );
     });
   }
+}
+
+class HomeScreen extends StatelessWidget {
+  // Access the ThemeController
+  final ThemeController themeController = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Daily Planner'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () {
+            // Toggle the theme when the button is pressed
+            themeController.toggleTheme();
+          },
+          child: Obx(() => Text(
+              themeController.isDarkTheme.value ? 'Switch to Light Theme' : 'Switch to Dark Theme')),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+// this is cool
+
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+
+// also cool
+
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       darkTheme: ThemeData.dark(useMaterial3: true),
+//       home: const MyHomePage(title: 'Daily Planner'),
